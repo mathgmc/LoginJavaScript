db51c3891a914bd3b5625559aa12848a07543c51
Matheus Carvalho <matheusgmc@hotmail.com>
2019-08-16 19:23:51 -0300

    update project name and add comments

--- users/routes.config.js
+++ users/routes.config.js
@@ -8,29 +8,39 @@ const PAID = config.permissionLevels.PAID_USER;
 const FREE = config.permissionLevels.NORMAL_USER;
 
 exports.routesConfig = function (app) {
+  
+    //Create User
     app.post('/users', [
         UsersController.insert
     ]);
+    //List All Users if you are PAID User
     app.get('/users', [
         ValidationMiddleware.validJWTNeeded,
         PermissionMiddleware.minimumPermissionLevelRequired(PAID),
         UsersController.list
     ]);
+
+    //List the userId specified if you are the same user or ADMIN
     app.get('/users/:userId', [
         ValidationMiddleware.validJWTNeeded,
         PermissionMiddleware.minimumPermissionLevelRequired(FREE),
         PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
         UsersController.getById
     ]);
+
+    //Update the userId specified if you are the same user or ADMIN
     app.patch('/users/:userId', [
         ValidationMiddleware.validJWTNeeded,
         PermissionMiddleware.minimumPermissionLevelRequired(FREE),
         PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
         UsersController.patchById
     ]);
+
+    //Delete the userId specified if you are ADMIN
     app.delete('/users/:userId', [
         ValidationMiddleware.validJWTNeeded,
         PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
         UsersController.removeById
     ]);
+
 };
